3050b33c65733758ba68b8bb775229e8392ca36c
Juho Juurinen <juho.juurinen@gmail.com>
2022-05-10 08:51:54 +0300

    exercise 10.1

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>I'm looking at this text on the screen of my OnePlus. What a time to be alive!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
